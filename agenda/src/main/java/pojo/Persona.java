6339092a02a7f375f777de74626aaded7f6de7d4
vitu71 <vitusanchez@gmail.com>
2018-09-10 18:13:37 +0200

    creada clase persona
    victor sanchez el mangurrian creador

--- agenda/src/main/java/pojo/Persona.java
+++ agenda/src/main/java/pojo/Persona.java
@@ -1,7 +1,69 @@
 package pojo;
+import java.sql.Date;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name="personas")
 public class Persona {
+	private int idpersonas;
+	private String nombre;
+	private String apellido1;
+	private String apellido2;
+	private String dni;
+	private String fechadeNacimiento;
+	private int idEmpleado;
+	
+	@Id
+	@GeneratedValue
+	@Column(name="idpersonas")
+	
+	public int getIdpersonas() {
+		return idpersonas;
+	}
+	public void setIdpersonas(int idpersonas) {
+		this.idpersonas = idpersonas;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido1() {
+		return apellido1;
+	}
+	public void setApellido1(String apellido1) {
+		this.apellido1 = apellido1;
+	}
+	public String getApellido2() {
+		return apellido2;
+	}
+	public void setApellido2(String apellido2) {
+		this.apellido2 = apellido2;
+	}
+	public String getDni() {
+		return dni;
+	}
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+	public String getFechadeNacimiento() {
+		return fechadeNacimiento;
+	}
+	public void setFechadeNacimiento(String fechadeNacimiento) {
+		this.fechadeNacimiento = fechadeNacimiento;
+	}
+	public int getIdEmpleado() {
+		return idEmpleado;
+	}
+	public void setIdEmpleado(int idEmpleado) {
+		this.idEmpleado = idEmpleado;
+	}
+	
 
 }
