48d6cb1a142bb7c9d33812fcdbe543ba3cd42ba8
Juan Carlos <juanca5121996@gmail.com>
2018-09-12 19:35:29 +0200

    Nada

--- agenda/src/main/java/com/example/agenda/controller/AppController.java
+++ agenda/src/main/java/com/example/agenda/controller/AppController.java
@@ -4,6 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -59,11 +62,11 @@ public class AppController {
 		return new ModelAndView("redirect:/");		
 	}*/
 	
-	//@RequestMapping(value = "/save", method = RequestMethod.POST)
-	/*public ModelAndView saveUser(@ModelAttribute User user) {
+	@RequestMapping(value = "/save", method = RequestMethod.POST)
+	public ModelAndView saveUser(@ModelAttribute("persona") Personas persona) {
 		logger.info("-- en SAVE");
-		userService.add(user);
-		return new ModelAndView("redirect:/");
-	}*/
+		iService.add(persona);
+		return new ModelAndView("redirect:/list");
+	}
 	
 }
