174906c127e67c4408476bc2aaa824ca60df912d
Ed <uhrgur@gmail.com>
2018-09-13 14:49:51 +0200

    No gran cosa

--- agenda/src/main/java/com/example/agenda/controller/AppController.java
+++ agenda/src/main/java/com/example/agenda/controller/AppController.java
@@ -1,30 +1,20 @@
 package com.example.agenda.controller;
 
 import java.util.List;
-
 import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
-
 import com.example.agenda.pojos.Personas;
 import com.example.agenda.service.IService;
-import com.example.agenda.service.ServiceImpl;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import com.example.agenda.pojos.Personas;
 
 
 
