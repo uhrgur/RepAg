be00826e674770b1d94321ccf57abe50cf4479ea
vitu71 <vitusanchez@gmail.com>
2018-09-13 15:12:37 +0200

    Prueba unitaria
    Subida la prueba unitaria.

---
+++ agenda/src/test/java/com/example/agenda/AgendaApplicationTest03HttpRequest.java
@@ -0,0 +1,30 @@
+package com.example.agenda;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class AgendaApplicationTest03HttpRequest {
+	
+	
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void greetingShouldReturnDefaultMessage() throws Exception {
+        assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/list",
+                String.class)).contains("Nombre"); // comprobamos que nos arranca el servidor y comprueba que la salida contiene el String nombre.
+    }
+}
